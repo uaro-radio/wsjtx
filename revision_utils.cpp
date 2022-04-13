fb76082b95333f28bde3162e6d6766578bec57ef
Uwe Risse <dg2ycb@gmx.de>
2022-04-13 12:53:52 +0200

    Promote revision ID to [2f518a5].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "ce5ecaf";
+  return "1da80c0";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
