e89bd85d10ff26192a209b812a197bc078906793
Uwe Risse <dg2ycb@gmx.de>
2022-05-08 10:45:35 +0200

    Optimize method and use it also for displaying bands in BlankLine [f021997].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "c7e7062";
+  return "cac0ea4";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
