dbaebd999ab758d0a9276efa1b023cc7a7591bfa
Uwe Risse <dg2ycb@gmx.de>
2022-06-03 12:03:05 +0200

    Update user guide, release notes, NEWS, etc.; align with WSJT-X v2.6.0-rc1 source code until [365efd0].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "f1497fb";
+  return "f77eb1d";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
