a72962415d3f7e2b33a3a633b0e9b8ab7c0fddaa
Uwe Risse <dg2ycb@gmx.de>
2022-06-13 21:02:33 +0200

    Change release date to June 20, 2022, and version ID to "6744bc".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "121091";
+  return "6744bc";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
