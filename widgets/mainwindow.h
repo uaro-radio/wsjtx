50bb0195e073131e7491f01e8b2d7156a09aeed9
Uwe Risse <dg2ycb@gmx.de>
2022-06-26 11:03:05 +0200

    Update standard files to the latest release.

--- widgets/mainwindow.h
+++ widgets/mainwindow.h
@@ -150,8 +150,6 @@ private slots:
   void on_msk144Button_clicked();
   void on_q65Button_clicked();
   void on_jt65Button_clicked();
-  void on_fst4Button_clicked();
-  void on_wsprButton_clicked();
   void on_tx1_editingFinished();
   void on_tx2_editingFinished();
   void on_tx3_editingFinished();
