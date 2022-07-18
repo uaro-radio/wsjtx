69e3a411237f1a4350e64a03cae85941c933600a
Uwe Risse <dg2ycb@gmx.de>
2022-07-18 08:34:57 +0200

    If DX grid is blank, set DX astro params to zero. Do this in astrosub.f90, not asstro.cpp [5c5daa0].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "c296bc";
+  return "2e4a6c";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
