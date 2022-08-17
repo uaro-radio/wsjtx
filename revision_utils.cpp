699d9826757e6ab2e4f985e124c41f1ccf10030c
Uwe Risse <dg2ycb@gmx.de>
2022-08-17 10:17:05 +0200

    Add v2.6.0-rc3 NEWS and Release Notes, set Revision ID to "a483da".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "f683d4";
+  return "a483da";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
