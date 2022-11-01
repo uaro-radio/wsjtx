c208f7ba989f4357908d9d07ccfbfe6e0551ec88
Uwe Risse <dg2ycb@gmx.de>
2022-11-01 14:57:21 +0100

    Suppress "Transmit digital gain" overlay unless PWR attenuator is being adjusted.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "1bcbca";
+  return "bc8ee6";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
