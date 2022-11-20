4663c4ae0615ae71ef4b76d78619b4e0805ab653
Uwe Risse <dg2ycb@gmx.de>
2022-11-20 10:31:22 +0100

    Set revision ID to "b3f6d4".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "a247bc";
+  return "b3f6d4";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
