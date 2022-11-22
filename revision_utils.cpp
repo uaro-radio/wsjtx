22290dfd38c03a9601ed483cacd818ce4a433fb1
Uwe Risse <dg2ycb@gmx.de>
2022-11-23 00:32:52 +0100

    Updates to NEWS and Release notes [6d853bc].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "fb0122";
+  return "f77a24";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
