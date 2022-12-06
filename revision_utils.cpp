4754dedb3c95ea8ac70365c4da56079ba7d7f6e0
Uwe Risse <dg2ycb@gmx.de>
2022-12-06 16:22:19 +0100

    Set revision ID to "c6d13a".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "49a7e9";
+  return "c6d13a";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
