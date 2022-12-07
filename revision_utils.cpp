f32476a9bff44cc62a25a969a16149f92720988d
Uwe Risse <dg2ycb@gmx.de>
2022-12-07 12:13:59 +0100

    Set revision ID to "f21d2c".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "c6d13a";
+  return "f21d2c";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
