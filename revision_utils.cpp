e337d302abdf3542410cbbe81aedd75b3e20438b
Uwe Risse <dg2ycb@gmx.de>
2022-12-29 16:06:44 +0100

    PACC support: Allow some more exchanges for FT RU contest mode + Update  ARRL section abbreviations, NEWS and Release Notes [15e3beb].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "0bf83a";
+  return "a6018b";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
