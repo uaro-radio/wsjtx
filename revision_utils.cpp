dfcb7f2ceaad7c0bdfdd1b3a96cfcd6643d645a4
Uwe Risse <dg2ycb@gmx.de>
2022-12-29 20:16:23 +0100

    Update the User Guide.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "a4b2b4";
+  return "5c1adb";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
