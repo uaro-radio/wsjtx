205b8afbad951be3d6d79e3a5cf1519dd5d3e69c
Uwe Risse <dg2ycb@gmx.de>
2023-03-15 17:30:31 +0100

    Better size policy for the "F Tol" spinner control [c7df0f1].

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "df3e92";
+  return "b8b0b7";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
