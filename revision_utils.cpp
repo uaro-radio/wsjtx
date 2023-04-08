9de7be9d9ab47b14cb98371c5af5aebf4524ea39
Uwe Risse <dg2ycb@gmx.de>
2023-04-08 14:42:38 +0200

    Update to WSJT-X source code [5cf1198]. Re-activate use of MaxDrift for late-stage decoding.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "69f9c1";
+  return "c34879";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
