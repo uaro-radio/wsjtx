079030ed38176c5072f425b00f29308290d418b2
Uwe Risse <dg2ycb@gmx.de>
2023-05-09 14:39:17 +0200

    Update to WSJT-X "7bb16b" code;

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "c34879";
+  return "7bb16b";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
