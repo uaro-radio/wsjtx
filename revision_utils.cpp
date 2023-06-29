2d7988c111bc96d61865f88ed608ca302579c4e2
Uwe Risse <dg2ycb@gmx.de>
2023-06-29 18:57:57 +0200

    Update to WSJT-X v2.7.0-rc2 official code.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "230620-09";
+  return "466792";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
