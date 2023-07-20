d522cd24d5f31a36b6cb958afc993f2812775188
Uwe Risse <dg2ycb@gmx.de>
2023-07-20 17:23:56 +0200

    Revert "For Q65, don't display decodes within 10 Hz of fQSO in the right window, but still display all messages for us."
    This reverts commit 595b5bf0c197fa882576e2b54a02e502591b3c77.

--- widgets/mainwindow_iws.cpp
+++ widgets/mainwindow_iws.cpp
@@ -5357,9 +5357,10 @@ void MainWindow::readFromStdout()                             //readFromStdout
 
         }
       } else {
-        if((abs(audioFreq - m_wideGraph->rxFreq()) <= 10) and (!(m_mode=="Q65" and !bAvgMsg)) and
+        if((abs(audioFreq - m_wideGraph->rxFreq()) <= 10) and
            !m_config.enable_VHF_features()) bDisplayRight=true;
       }
+      if(m_mode=="Q65" and !bAvgMsg) bDisplayRight=false;
 
       if (bDisplayRight && !block_right_display) {
         // This msg is within 10 hertz of our tuned frequency, or a JT4 or JT65 avg,
