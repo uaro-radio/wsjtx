dfc3efdee5cd44695abaa2e8e9a98480f5d5996e
Uwe Risse <dg2ycb@gmx.de>
2023-10-03 11:39:14 +0200

    Set version number to "231001-FD02-SM02".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "231001-FD02-SM01";
+  return "231001-FD02-SM02";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
