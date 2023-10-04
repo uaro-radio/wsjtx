c05f5d4f25d6e38292f7db53caa911ea52345c84
Uwe Risse <dg2ycb@gmx.de>
2023-10-04 10:23:47 +0200

    Update cty.dat file (French Polynesia corrected).

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "231003-FD02-SM02";
+  return "231004";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
