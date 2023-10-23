1dc7dc8c3e54f5bbbc8972964fa8ffbf76e5bee8
Uwe Risse <dg2ycb@gmx.de>
2023-10-23 20:09:26 +0200

    Improve new function "Update Hamlib (Windows only)".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "231020";
+  return "231024";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
