0a943ee46b7231f86cc468ca681d8054ccb8d2d2
Uwe Risse <dg2ycb@gmx.de>
2024-03-07 11:19:10 +0100

    Preparations for the upcoming RC4.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "240229";
+  return "240311-rc4";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
