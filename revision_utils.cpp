b40b4f9e63afaf72b29a6148f61fc477ba71a574
Uwe Risse <dg2ycb@gmx.de>
2024-03-18 10:05:29 +0100

    Insert blank line in Rx Frequency window after band change.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "240311-rc4";
+  return "240317";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
