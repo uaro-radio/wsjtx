aa1d0d9e04a7295842fc39458021c266fd09292b
Uwe Risse <dg2ycb@gmx.de>
2024-05-02 15:42:51 +0200

    Add Help menu item "Recommended audio settings".

--- widgets/mainwindow__Alerts_AL_PLUS.h
+++ widgets/mainwindow__Alerts_AL_PLUS.h
@@ -197,6 +197,7 @@ private slots:
   void on_actionQuick_Start_Guide_to_WSJT_X_2_7_and_QMAP_triggered();
   void on_actionWSJT_X_improved_Home_Page_triggered();
   void on_actionThe_additional_features_of_wsjt_x_improved_triggered();
+  void on_actionRecommended_Audio_Settings_triggered();
   void on_actionOnline_User_Guide_triggered();
   void on_actionLocal_User_Guide_triggered();
   void on_actionWide_Waterfall_triggered();
