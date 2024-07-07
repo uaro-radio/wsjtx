d8fe70c23e0029f4ad33e266f4422bfdb514dcad
Uwe Risse <dg2ycb@gmx.de>
2024-07-07 17:50:18 +0200

    Set revision ID to "240707".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "240606";
+  return "240701-RC5";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
