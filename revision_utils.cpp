6c223b92b3b902475f8fd8968f8efd87a36e831f
Uwe Risse <dg2ycb@gmx.de>
2024-09-18 17:33:30 +0200

    Set revision ID to "240918-RC7".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "240909-otp";
+  return "240917-otp";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
