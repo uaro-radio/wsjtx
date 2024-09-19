46a18131915b2382106669d53dfe2157095a60ab
Uwe Risse <dg2ycb@gmx.de>
2024-09-19 20:10:23 +0200

    Don't set FTol=20 in SuperFox mode. Remove the entire block as this appears to be not needed.

--- widgets/mainwindow_i.cpp
+++ widgets/mainwindow_i.cpp
@@ -4547,10 +4547,6 @@ void MainWindow::decode()                                       //decode()
   if(dec_data.params.b_superfox and dec_data.params.b_even_seq and m_ihsym<50) return;
 
   dec_data.params.ntol=ui->sbFtol->value ();
-  if(!m_config.enable_VHF_features()) {
-    dec_data.params.ntol=20;
-    dec_data.params.naggressive=0;
-  }
   if(m_mode=="FST4") {
     dec_data.params.ntol=ui->sbFtol->value();
     if(m_config.single_decode()) {
