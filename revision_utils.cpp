a742ce240bd1cf5344388b954aaf0c08f3005385
Uwe Risse <dg2ycb@gmx.de>
2024-09-22 09:03:03 +0200

    Update to BBsfox5b [885f4ba]: Add sfoxsim + More (final?) corrections to logic for handling SF free text messages.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "240917-otp";
+  return "240918-RC7";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
