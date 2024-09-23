3ac424c2767440a78563cea3ce9fc59fbb20f4aa
Uwe Risse <dg2ycb@gmx.de>
2024-09-23 22:18:31 +0200

    Fix an inconsistency with the WideGraph goal posts.

--- widgets/mainwindow_iws+_PLUS.cpp
+++ widgets/mainwindow_iws+_PLUS.cpp
@@ -3606,6 +3606,10 @@ void MainWindow::statusChanged()
     ui->sbNslots->setValue(m_Nslots0);
   }
   if (SpecOp::HOUND==m_specOp) ui->cbRxAll->setVisible(!m_config.superFox());
+  if ((SpecOp::HOUND!=m_specOp && SpecOp::FOX!=m_specOp) or !m_config.superFox()) {
+    m_wideGraph->setSuperFox(false);
+    m_wideGraph->setSuperHound(false);
+  }
   check_button_color();
 }
 
