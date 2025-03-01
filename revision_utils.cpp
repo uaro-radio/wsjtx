e0e3a1437c6d1be78d873042f6a324af2ff482f1
Uwe Risse <dg2ycb@gmx.de>
2025-03-01 10:57:32 +0100

    Update User Guide to status of WSJT-X Improved 2.8 GA.

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "250206";
+  return "250301";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
