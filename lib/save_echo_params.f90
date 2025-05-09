b7b14e9b365ace4ed00209792fdf054bb1471e11
Joe Taylor <joe@princeton.edu>
2025-05-09 12:12:35 -0400

    Save itone(1:6) to Echo mode .wav files.

--- lib/save_echo_params.f90
+++ lib/save_echo_params.f90
@@ -1,14 +1,16 @@
 subroutine save_echo_params(nDopTotal,nDopAudio,nfrit,f1,fspread,ntonespacing, &
-     id2,idir)
+     itone,id2,idir)
 
-  integer*2 id2(12)
-  integer*2 id2a(12)
+  integer*2 id2(15),id2a(15)
+  integer*4 itone(6)
+  integer*1 itone0(6)
   equivalence (nDopTotal0,id2a(1))
   equivalence (nDopAudio0,id2a(3))
   equivalence (nfrit0,id2a(5))
   equivalence (f10,id2a(7))
   equivalence (fspread0,id2a(9))
   equivalence (ntonespacing0,id2a(11))
+  equivalence (itone0,id2a(13))
   
   if(idir.gt.0) then
      nDopTotal0=nDopTotal
@@ -17,10 +19,11 @@ subroutine save_echo_params(nDopTotal,nDopAudio,nfrit,f1,fspread,ntonespacing, &
      f10=f1
      fspread0=fspread
      ntonespacing0=ntonespacing
+     itone0=itone
      id2=id2a
   else
      id2a=id2
-     id2(1:10)=0
+     id2(1:15)=0
      nDopTotal=nDopTotal0
      nDopAudio=nDopAudio0
      nfrit=nfrit0
@@ -29,6 +32,8 @@ subroutine save_echo_params(nDopTotal,nDopAudio,nfrit,f1,fspread,ntonespacing, &
      if(ntonespacing0.ne.5 .and. ntonespacing0.ne.10 .and.                    &
           ntonespacing0.ne.20 .and. ntonespacing0.ne.50) ntonespacing0=10
      ntonespacing=ntonespacing0
+     if(any(itone0.lt.0) .or. any(itone0.gt.36)) itone0=0
+     itone=itone0
   endif
 
   return
