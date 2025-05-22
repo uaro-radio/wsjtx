dc1c3c59d06289aca0f96e89813855a866235d1d
Uwe Risse <dg2ycb@gmx.de>
2025-05-22 12:07:14 +0200

    Add "dev" to revision ID

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "250522";
+  return "dev 250522";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
