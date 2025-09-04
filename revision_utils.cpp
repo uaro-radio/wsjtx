3bf342cebbcafc1bf6c3bccfb0da55030b1fe07f
Uwe Risse <dg2ycb@gmx.de>
2025-09-04 20:17:40 +0200

    Preparations for the coming 3.0.0-rc1 250915 release

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "250904";
+  return "250915-RC1";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
