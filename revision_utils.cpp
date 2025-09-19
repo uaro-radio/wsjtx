c4ff0ac7b4221e081a2c873215dd7037b7a4aa81
Uwe Risse <dg2ycb@gmx.de>
2025-09-19 15:42:00 +0200

    Set revision ID to "250919-001".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "250915-RC1";
+  return "250919-001";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
