f9a917005e5f7c79a85ad8f43ad52a4be332d4f0
Uwe Risse <dg2ycb@gmx.de>
2025-10-26 13:10:44 +0100

    Update CMakeLists_wsjtx.txt, and set the revision ID to "251026".

--- revision_utils.cpp
+++ revision_utils.cpp
@@ -26,7 +26,7 @@ namespace
 
 QString revision (QString const& scs_rev_string)
 {
-  return "251022";
+  return "251020";
   QString result;
   auto revision_from_scs = revision_extract_number (scs_rev_string);
 
