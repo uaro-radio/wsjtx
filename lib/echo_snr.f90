ca0951e6568f7d45e4fb5f8c9c9fd4c3f1f37f5b
Joe Taylor <joe@princeton.edu>
2025-11-04 09:25:37 -0500

    Revert "Add diagnostics for Echo SNR testing."
    This reverts commit 2655accfae23486e0a060b90bbd56c50338af700.

--- lib/echo_snr.f90
+++ lib/echo_snr.f90
@@ -1,15 +1,12 @@
-subroutine echo_snr(sa,sb,bDiskData,nc,xdt,fspread,blue,red,snrdb,db_err,fpeak,snr_detect)
+subroutine echo_snr(sa,sb,fspread,blue,red,snrdb,db_err,fpeak,snr_detect)
 
   parameter (NZ=4096)
   real sa(NZ)
   real sb(NZ)
   real blue(NZ)
   real red(NZ)
-  logical*1 bDiskData
-  integer it(8)
   integer ipkv(1)
   equivalence (ipk,ipkv)
-
 
   df=12000.0/32768.0
   wh=0.5*fspread+10.0
@@ -18,20 +15,18 @@ subroutine echo_snr(sa,sb,bDiskData,nc,xdt,fspread,blue,red,snrdb,db_err,fpeak,s
   i3=nint((1500.0 + wh)/df) - 2048
   i4=nint((1500.0 + 2.0*wh)/df) - 2048
 
+!  call pctile(sb(i1),i2-i1,50,r0)
+!  call pctile(sb(i3+1),i4-i3,50,r1)
+!  ave=0.5*(r0+r1)
+!  blue=sa/ave
+!  red=sb/ave
+
   baseline=(sum(sb(i1:i2-1)) + sum(sb(i3+1:i4)))/(i2+i4-i1-i3)
   blue=sa/baseline
   red=sb/baseline
   psig=sum(red(i2:i3)-1.0)
   pnoise_2500 = 2500.0/df
   snrdb=db(psig/pnoise_2500)
-
-  if(nc.eq.1 .and. .not.bDiskData) then
-     call date_and_time(values=it)
-     it(5)=it(5)-it(4)/60
-     write(*,3001) it(5:7),i1,i2,i3,i4,xdt,fspread,snrdb,  &
-       psig,pnoise_2500,baseline
-3001 format(3i2.2,4i6,3f8.2,3f10.1)
-  endif
 
   smax=0.
   mh=max(1,nint(0.2*fspread/df))
