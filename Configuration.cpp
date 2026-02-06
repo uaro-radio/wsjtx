b9a901dc6ab04dfdaf37bb970d54a5430da9b147
Uwe Risse <dg2ycb@gmx.de>
2026-02-06 14:48:29 +0100

    Update URLs for downloading the libhamlib-4.dll files.

--- Configuration.cpp
+++ Configuration.cpp
@@ -3872,14 +3872,12 @@ void Configuration::impl::on_hamlib_download_button_clicked (bool /*clicked*/)
   ui_->revert_update_button->setEnabled (false);
   if (ui_->rbHamlib32->isChecked()) {
     cty_download.configure(network_manager_,
-//                           "https://hamlib.sourceforge.net/snapshots/dll32/libhamlib-4.dll",  // new hamlib download location
-                           "http://dr-risse-consulting.de/hamlib/dll32/libhamlib-4.dll",  // hamlib mirror DG2YCB
+                           "https://hamlib.sourceforge.net/snapshots-4.7/dll32/libhamlib-4.dll",  // new hamlib download location
                            dataPath.absoluteFilePath("libhamlib-4_new.dll"),
                            "Downloading latest libhamlib-4.dll");
   } else {
     cty_download.configure(network_manager_,
-//                           "https://hamlib.sourceforge.net/snapshots/dll64/libhamlib-4.dll",  // new hamlib download location
-                           "http://dr-risse-consulting.de/hamlib/dll64/libhamlib-4.dll",  // hamlib mirror DG2YCB
+                           "https://hamlib.sourceforge.net/snapshots-4.7/dll64/libhamlib-4.dll",  // new hamlib download location
                            dataPath.absoluteFilePath("libhamlib-4_new.dll"),
                            "Downloading latest libhamlib-4.dll");
   }
