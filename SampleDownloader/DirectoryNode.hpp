f44d3ca3105df54b49314a641d1bc61b5d54e151
Roger Rehr <73w3sz@gmail.com>
2026-04-06 01:51:53 -0400

    Fix to checkbox behavior in SampleDownloader GUI

--- SampleDownloader/DirectoryNode.hpp
+++ SampleDownloader/DirectoryNode.hpp
@@ -24,7 +24,7 @@ public:
   explicit DirectoryNode (QTreeWidgetItem * parent, QString const& name)
     : QTreeWidgetItem {parent, Type}
   {
-    setFlags (flags () | Qt::ItemIsUserCheckable | Qt::ItemIsUserTristate);
+    setFlags (flags () | Qt::ItemIsUserCheckable | Qt::ItemIsTristate);
     setText (0, name);
     setCheckState (0, Qt::Unchecked);
 
